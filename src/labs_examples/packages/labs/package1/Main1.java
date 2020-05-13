01061a0c2828289ccd5beb1ce5f706ea24cb2cb8
farida2 <fari_belh@yahoo.co.uk>
2020-05-13 01:03:26 -0400

    working on packages 2

--- src/labs_examples/packages/labs/package1/Main1.java
+++ src/labs_examples/packages/labs/package1/Main1.java
@@ -1,4 +1,13 @@
 package labs_examples.packages.labs.package1;
 
+import labs_examples.packages.labs.package2.MyClass2;
+
 public class Main1 {
+    public static void main(String[] args) {
+        MyClass2 obj = new MyClass2(9,2);
+
+
+
+
+    }
 }
--- src/labs_examples/packages/labs/package2/Main2.java
+++ src/labs_examples/packages/labs/package2/Main2.java
@@ -1,4 +1,8 @@
 package labs_examples.packages.labs.package2;
+import labs_examples.packages.labs.package1.MyClass1;
 
 public class Main2 {
+    public static void main(String[] args) {
+       //double x = divide(9,3);
+    }
 }
