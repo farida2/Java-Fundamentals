f390ee060b315c1154b95faa5cd2792fe1d038e6
farida2 <fari_belh@yahoo.co.uk>
2020-05-18 00:12:06 -0400

    Adding REPL:Extending Thread

---
+++ src/labs_examples/multi_threading/labs/CreateNewThread2.java
@@ -0,0 +1,35 @@
+package labs_examples.multi_threading.labs;
+
+public class CreateNewThread2 {
+    public static void main(String[] args) {
+
+        // Step 1) demonstrate creating a new Thread using the MyThread class below
+        MyThread thread1 = new MyThread("First Thread");
+
+        // Step 2) demonstrate creating (and starting) a new Thread using the MyOtherThread class below
+        MyOtherThread thread2 = new MyOtherThread();
+        thread2.run();
+
+    }
+}
+
+class MyThread extends Thread {
+
+    public MyThread(String name){
+        super(name);
+        start();
+    }
+
+    @Override
+    public void run(){
+        System.out.println("I'm in a thread name " + getName() + "!");
+    }
+}
+
+class MyOtherThread extends Thread {
+
+    @Override
+    public void run(){
+        System.out.println("I'm in a thread name " + getName() + "!");
+    }
+}
