47edea4d1445dc1d642b77bc17c0318d8137d635
farida2 <fari_belh@yahoo.co.uk>
2020-05-22 16:52:27 -0400

    First Commit for Genirics

--- src/labs_examples/generics/labs/Exercise_01.java
+++ src/labs_examples/generics/labs/Exercise_01.java
@@ -6,5 +6,46 @@ package labs_examples.generics.labs;
  *      1.) Write a generic class with at least two instance variables, a constructor, and getter and setter methods.
  *      2.) Create a few objects of your generic class with different data types to demonstrate it's
  *          dynamic usage.
+ *
  */
+class TwoInstanceVarGen<T, V>{
+    T var1;
+    V var2;
+
+    public TwoInstanceVarGen(T var1, V var2) {
+        this.var1 = var1;
+        this.var2 = var2;
+    }
+
+    public T getVar1() {
+        return var1;
+    }
+
+    public void setVar1(T var1) {
+        this.var1 = var1;
+    }
+
+    public V getVar2() {
+        return var2;
+    }
+
+    public void setVar2(V var2) {
+        this.var2 = var2;
+    }
+}
+/*
+ * Create a few objects of your generic class with different data types to demonstrate it's
+ *          dynamic usage.
+ *
+ */
+
+class TwoInstanceVarGenDemo{
+    public static void main(String[] args) {
+        TwoInstanceVarGen<String, Integer> obj = new TwoInstanceVarGen("Year", 2020);
+        String v1 = obj.getVar1();
+        Integer v2 = obj.getVar2();
+        System.out.println(v1 +":" + v2);
+
+    }
+}
 
