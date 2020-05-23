2cdf624a9411908b8ae8cac4b404742fe974d543
farida2 <fari_belh@yahoo.co.uk>
2020-05-22 23:17:30 -0400

    an other commit

--- src/labs_examples/generics/labs/Exercise_02.java
+++ src/labs_examples/generics/labs/Exercise_02.java
@@ -1,8 +1,28 @@
 package labs_examples.generics.labs;
 
+import java.util.ArrayList;
+
 /**
  * Generics Exercise 2:
  *
  *      Create a class with a generic method that takes in an ArrayList of any Numeric type and returns the sum of all
  *      Numbers in the ArrayList
  */
+
+class ClassWithGenericMethod {
+    public  int genSum(ArrayList<? extends Number> arr){
+
+        int sum = 0;
+
+        for(int i = 0; i < arr.size(); i++){
+            sum += arr.indexOf(i);
+        }
+        return sum;
+    }
+}
+class GenSumDemo{
+    public static void main(String[] args) {
+        ArrayList<Number> arr = new ArrayList<>();
+
+    }
+}
